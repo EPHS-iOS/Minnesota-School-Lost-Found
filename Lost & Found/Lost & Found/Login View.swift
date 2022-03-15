2812ec29a8b485cc8010b37b9e07da9c7785cd16
Lucas B <lsbarkmeier@gmail.com>
2022-03-15 14:19:01 -0500

    new

---
+++ Lost & Found/Lost & Found/Login View.swift
@@ -0,0 +1,20 @@
+//
+//  Login View.swift
+//  Lost & Found
+//
+//  Created by 64008786 on 3/15/22.
+//
+
+import SwiftUI
+
+struct Login_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Login_View()
+    }
+}
