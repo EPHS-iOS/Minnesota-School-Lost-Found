fb08138cabd454123e8af9b67b3b6eaee1249d62
Lucas B <lsbarkmeier@gmail.com>
2022-03-15 13:57:02 -0500

    Main
    Epic

---
+++ Lost & Found/Lost & Found/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lost & Found
+//
+//  Created by 64008786 on 3/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
