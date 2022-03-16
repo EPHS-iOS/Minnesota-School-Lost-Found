7a729b02e83f426fb913ffad5baa69cafcb00b72
Lucas B <lsbarkmeier@gmail.com>
2022-03-16 15:11:42 -0500

    cool

--- LostFound/LostFound/View/ContentView.swift
+++ LostFound/LostFound/View/ContentView.swift
@@ -9,11 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Enter") {
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-        }
-    }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- LostFound/LostFound/View/LoginView.swift
+++ LostFound/LostFound/View/LoginView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var schoolCode : String = ""
+    
     var body: some View {
+        Text("Enter School Code")
+        TextField("School Code", text: $schoolCode)
+        Spacer()
         Button("Enter") {
             /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
         }
     }
 }
-
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
