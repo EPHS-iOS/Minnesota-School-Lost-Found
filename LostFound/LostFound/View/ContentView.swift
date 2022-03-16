50ab8620a1fefe1476a22277b144e784c071fb0d
maxj723 <46984489+maxj723@users.noreply.github.com>
2022-03-16 15:11:14 -0500

    added updating view thing

--- LostFound/LostFound/View/ContentView.swift
+++ LostFound/LostFound/View/ContentView.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Copy of LostItemsView here to view for testing
+    @StateObject var model = ItemModel()
+    
+
+    
     var body: some View {
-        Button("Enter") {
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-        }
+        
+        NavigationView {
+            VStack(alignment: .leading){
+                
+                ScrollView{
+                    GeometryReader{ geo in
+                        LazyVGrid(columns: [
+                            GridItem(.flexible()),
+                            GridItem(.flexible()),
+                            GridItem(.flexible())
+                        ], spacing: 3 ){
+                            ForEach(model.items, id: \.self){ item in
+                                NavigationLink(destination: IndividualItemView(item: item), label: {
+                                    Image(systemName: item.image)
+                                        .frame(width: geo.size.width/3, height: geo.size.width/3)
+                                        .background(Color.blue)
+                                        .foregroundColor(.white)
+                                    
+                                })
+                                
+                            }
+                        }
+                    }
+                }.navigationTitle(Text("Lost Items"))
+                    .navigationBarTitleDisplayMode(.automatic)
+//                    .onAppear {
+//                        model.items = model.items
+//                    }
+                NavigationLink(destination: AddLostItemView(), label: { Text("Add item") })
+            }
+        }.navigationViewStyle(.stack)
+            .environmentObject(model)
+        
     }
 }
 
