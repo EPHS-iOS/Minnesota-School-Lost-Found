208dfcb3531e14dde3bd87148223f68337b0588d
Lucas B <lsbarkmeier@gmail.com>
2022-03-16 14:07:02 -0500

    main
    cool

---
+++ LostFound/LostFound/LostFoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostFoundApp.swift
+//  LostFound
+//
+//  Created by 64008786 on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct LostFoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
