1da2c3fbb775282e980b1439e590dcba216d90f7
Ariel Thunstrom <94394689+ArielThunstrom@users.noreply.github.com>
2022-03-16 20:46:17 -0500

    changed UI

--- LostFound/LostFound/View/LoginView.swift
+++ LostFound/LostFound/View/LoginView.swift
@@ -12,8 +12,23 @@ struct LoginView: View {
     @State private var schoolCode : String = ""
     
     var body: some View {
+        
+        Text("Minnesota Schools")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+          
+        
+        Image("MN")
+     
+        Text("Lost & Found")
+            .font(.largeTitle)
         Text("Enter School Code")
+            .font(.headline)
+            .padding()
+            
+       
         TextField("School Code", text: $schoolCode)
+            .padding(.horizontal)
         Spacer()
         Button("Enter") {
             /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
