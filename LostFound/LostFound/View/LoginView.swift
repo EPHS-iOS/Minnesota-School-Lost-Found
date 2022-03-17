2daa6263f3c32dab60cc326099ecbc597dbc468f
Lucas B <lsbarkmeier@gmail.com>
2022-03-17 12:28:15 -0500

    change

--- LostFound/LostFound/View/LoginView.swift
+++ LostFound/LostFound/View/LoginView.swift
@@ -12,26 +12,28 @@ struct LoginView: View {
     @State private var schoolCode : String = ""
     
     var body: some View {
-        
-        Text("Minnesota Schools")
-            .font(.largeTitle)
-            .fontWeight(.bold)
-          
-        
-        Image("MN")
-     
-        Text("Lost & Found")
-            .font(.largeTitle)
-        Text("Enter School Code")
-            .font(.headline)
-            .padding()
+        VStack {
+            Text("Minnesota Schools")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+              
             
-       
-        TextField("School Code", text: $schoolCode)
-            .padding(.horizontal)
-        Spacer()
-        Button("Enter") {
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            Image("MN")
+         
+            Text("Lost & Found")
+                .font(.largeTitle)
+            Text("Enter School Code")
+                .font(.headline)
+                .padding()
+                
+           
+            TextField("School Code", text: $schoolCode)
+                .textFieldStyle(.roundedBorder)
+            
+                .padding(.horizontal)
+            Button("Enter") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
         }
     }
 }
