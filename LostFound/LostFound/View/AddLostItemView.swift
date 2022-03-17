2bbdd635155fd5b1e8b98b70dd0b3047667551ab
Ariel Thunstrom <94394689+ArielThunstrom@users.noreply.github.com>
2022-03-17 14:37:00 -0500

    changed UI
    still need to fix spacing

--- LostFound/LostFound/View/AddLostItemView.swift
+++ LostFound/LostFound/View/AddLostItemView.swift
@@ -23,77 +23,89 @@ struct AddLostItemView: View {
     
     
     var body: some View {
-        
-        VStack {
-            
-            ZStack(alignment: .bottomTrailing) {
-                Button (action: {
-                    changeProfileImage = true
-                    openCameraRoll = true
-                }, label: {
-                    if changeProfileImage {
-                        Image(uiImage: imageSelected)
-                            .frame(width: 300, height: 300)
-                            .foregroundColor(.white)
-                            .background(Color.gray)
-                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    } else {
-                        Image(systemName: "photo")
-                            .frame(width: 300, height: 300)
-                            .foregroundColor(.white)
-                            .background(Color.gray)
-                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                VStack {
+                    //(alignment: .bottomTrailing)
+                    ZStack {
+                        VStack {
+                            Button (action: {
+                                changeProfileImage = true
+                                openCameraRoll = true
+                            }, label: {
+                                if changeProfileImage {
+                                    Image(uiImage: imageSelected)
+                                        .frame(width: 300, height: 300)
+                                        .foregroundColor(.white)
+                                        .background(Color.gray)
+                                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                } else {
+                                    Image(systemName: "photo")
+                                        .frame(width: 300, height: 300)
+                                        .foregroundColor(.white)
+                                        .background(Color.gray)
+                                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                }
+                            })//.sheet(isPresented: $openCameraRoll) {
+                            //   ImagePicker(selectedImage: $imageSelected, sourceType: .photoLibrary)
+                            // }
+                            Spacer()
+                            ZStack {
+                                Picker("Use Camera", selection: $useCamera, content: {
+                                    HStack {
+                                        Image(systemName: "camera")
+                                        Text("Camera")
+                                    }.tag(true)
+                                    HStack {
+                                        Image(systemName: "photo.on.rectangle")
+                                        Text("Photo Library")
+                                    }.tag(false)
+                                }).labelsHidden()
+                                
+            //                    Image(systemName: "pencil")
+            //                        .frame(width: 30, height: 30)
+            //                        .foregroundColor(.white)
+            //                        .background(Color.gray)
+            //                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            }
+                        }
+                    }.sheet(isPresented: $openCameraRoll) {
+                        if useCamera {
+                            ImagePicker(selectedImage: $imageSelected, sourceType: .camera)
+                        } else {
+                            ImagePicker(selectedImage: $imageSelected, sourceType: .photoLibrary)
+                        }
                     }
-                })//.sheet(isPresented: $openCameraRoll) {
-                //   ImagePicker(selectedImage: $imageSelected, sourceType: .photoLibrary)
-                // }
-                ZStack {
-                    Picker("Use Camera", selection: $useCamera, content: {
-                        HStack {
-                            Image(systemName: "camera")
-                            Text("Camera")
-                        }.tag(true)
-                        HStack {
-                            Image(systemName: "photo.on.rectangle")
-                            Text("Photo Library")
-                        }.tag(false)
-                    }).labelsHidden()
                     
-//                    Image(systemName: "pencil")
-//                        .frame(width: 30, height: 30)
-//                        .foregroundColor(.white)
-//                        .background(Color.gray)
-//                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                }
-            }.sheet(isPresented: $openCameraRoll) {
-                if useCamera {
-                    ImagePicker(selectedImage: $imageSelected, sourceType: .camera)
-                } else {
-                    ImagePicker(selectedImage: $imageSelected, sourceType: .photoLibrary)
+                    Spacer()
+                    Text("Item lost")
+                    TextField("Item Lost", text: $enteredTitle)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    //Spacer()
+                    //not sure if we need this 
+                    /*
+                    Text("Does it have a name on it?")
+                    TextField("Name", text: $enteredName)
+                     
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                     */
+                    //Spacer()
+                    Text("Description")
+                    TextField("Description", text: $enteredDescription)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    //Spacer()
+                    Button {
+                        itemModel.addItem(image: imageSelected, title: enteredTitle, addedDate: "03/16/2022", isClaimed: false, name: enteredName, description: enteredDescription)
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text("Add Item")
+                    }
+                    
                 }
             }
-            
-            Spacer()
-            Text("Item lost")
-            TextField("Item Lost", text: $enteredTitle)
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Spacer()
-            Text("Does it have a name on it?")
-            TextField("Name", text: $enteredName)
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            //Spacer()
-            Text("Description")
-            TextField("Description", text: $enteredDescription)
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            //Spacer()
-            Button {
-                itemModel.addItem(image: imageSelected, title: enteredTitle, addedDate: "03/16/2022", isClaimed: false, name: enteredName, description: enteredDescription)
-                presentationMode.wrappedValue.dismiss()
-            } label: {
-                Text("Add Item")
-            }
-            
         }
+        //change title to something else?
+        //.navigationBarTitle("Add Item")
     }
 }
 
